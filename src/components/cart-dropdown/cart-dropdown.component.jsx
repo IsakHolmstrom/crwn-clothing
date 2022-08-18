ef1ca4d55a6fb2aec393f11aa5ec6c8626eb9182
IsakHolmstrom <isak.holmstrom@rejlers.se>
2022-08-18 15:56:03 +0200

    created checkout page along with checkout items and checkout cart functonallity

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,4 +1,5 @@
 import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { CartContext } from '../../contexts/cart.context';
 
 import './cart-dropdown.styles.scss';
@@ -8,12 +9,18 @@ import CartItem from '../cart-item/cart-item.component';
 
 const CartDropdown = () => {
     const { cartItems } = useContext(CartContext);
+    const navigate = useNavigate();
+
+    const goTocheckOutHandler = () => {
+        navigate('/checkout')
+    }
+
     return(
         <div className='cart-dropdown-container'>
             <div className='cart-items' >
                 {cartItems.map(item => <CartItem key={item.id} cartItem={item}/>)}
             </div>
-            <Button>GO TO CHECKOUT</Button>
+            <Button onClick={goTocheckOutHandler} >GO TO CHECKOUT</Button>
         </div>
     )
 }
